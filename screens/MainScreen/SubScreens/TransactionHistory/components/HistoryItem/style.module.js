ac5c25ec890ab4d7ad433c42025cb475bac2b496
Azanamin <minhnhat912002@gmail.com>
2023-10-04 15:54:50 +0700

    add change password, transaction history screen

---
+++ screens/MainScreen/SubScreens/TransactionHistory/components/HistoryItem/style.module.js
@@ -0,0 +1,17 @@
+import { StyleSheet } from 'react-native'
+
+export const styles = StyleSheet.create({
+  container: {
+    flex: 0,
+    flexDirection: 'row',
+    width: '80%',
+    backgroundColor: '#E8DDDD',
+    alignItems: 'center',
+    alignSelf: 'center',
+    justifyContent: 'space-between',
+    padding: 10,
+    borderRadius: 5,
+  },
+  leftContainer: {},
+  rightContainer: {},
+})
---
+++ screens/MainScreen/SubScreens/TransactionHistory/style.module.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from 'react-native'
+
+export const styles = StyleSheet.create({
+  container: {
+    flex: 0,
+    width: '100%',
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingTop: 50,
+  },
+  historyContainer: {
+    width: '100%',
+  },
+})
