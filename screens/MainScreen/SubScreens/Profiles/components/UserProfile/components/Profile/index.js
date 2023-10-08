32fa74f63984e9b5c8b48c20413e8b24a1d2c0ff
Azanamin <minhnhat912002@gmail.com>
2023-10-08 15:56:10 +0700

    modify user profile UI

--- screens/MainScreen/SubScreens/Profiles/components/UserProfile/components/Profile/index.js
+++ screens/MainScreen/SubScreens/Profiles/components/UserProfile/components/Profile/index.js
@@ -13,37 +13,63 @@ export default function Profile({ navigation }) {
 
   return (
     <ScrollView contentContainerStyle={styles.container}>
-      <Avatar.Image source={defaultAvatar} size={200} />
-      <Text
-        style={{
-          fontSize: 20,
-          fontWeight: 600,
-        }}
+      <TouchableOpacity
+        style={styles.avatarContainer}
+        onPress={() => navigation.navigate(PROFILE_SCREEN.ACCOUNT_INFO)}
       >
-        {user.first_name + ' ' + user.last_name}
-      </Text>
-      <View style={styles.optionGroup}>
-        <TouchableOpacity
-          style={styles.optionItem}
-          onPress={() => navigation.navigate(PROFILE_SCREEN.ACCOUNT_INFO)}
+        <Avatar.Image source={defaultAvatar} size={50} />
+        <Text
+          style={{
+            fontSize: 20,
+            fontWeight: 600,
+          }}
         >
-          <IconButton icon="account" />
-          <Text style={{ fontSize: 18, fontWeight: 600 }}>Profile</Text>
+          {user.first_name + ' ' + user.last_name}
+        </Text>
+        <IconButton icon="chevron-right" style={{ marginLeft: 'auto' }} />
+      </TouchableOpacity>
+      <View style={styles.expertOption}>
+        <View style={styles.expertOptionColumn}>
+          <TouchableOpacity style={styles.expertOptionItem}>
+            <IconButton icon="human-male-board-poll" size={40} />
+            <Text style={{ fontSize: 18, fontWeight: 600 }}>Job List</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.expertOptionItem}>
+            <IconButton icon="history" size={40} />
+            <Text style={{ fontSize: 18, fontWeight: 600 }}>History</Text>
+          </TouchableOpacity>
+        </View>
+        <View style={styles.expertOptionColumn}>
+          <TouchableOpacity style={styles.expertOptionItem}>
+            <IconButton icon="party-popper" size={40} />
+            <Text style={{ fontSize: 18, fontWeight: 600 }}>Job Request</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.expertOptionItem}>
+            <IconButton icon="chart-bar" size={40} />
+            <Text style={{ fontSize: 18, fontWeight: 600 }}>Statitics</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+      <Divider style={{ width: '100%' }} bold />
+      <View style={styles.optionGroup}>
+        <TouchableOpacity style={styles.optionItem}>
+          <IconButton icon="cog" />
+          <Text style={{ fontSize: 18, fontWeight: 600 }}>Setting</Text>
         </TouchableOpacity>
         <Divider bold />
-        <TouchableOpacity
-          style={styles.optionItem}
-          onPress={() => navigation.navigate(PROFILE_SCREEN.CHANGE_PWD)}
-        >
-          <IconButton icon="lock-reset" />
-          <Text style={{ fontSize: 18, fontWeight: 600 }}>Change Password</Text>
+        <TouchableOpacity style={styles.optionItem}>
+          <IconButton icon="book-account-outline" />
+          <Text style={{ fontSize: 18, fontWeight: 600 }}>Term & Condition</Text>
         </TouchableOpacity>
       </View>
+      <Divider style={{ width: '100%' }} bold />
       <TouchableOpacity
         style={styles.signout}
         onPress={() => RootNavigate.navigate(SCREEN.HOME)}
       >
-        <Button icon="logout">Sign out</Button>
+        <Button icon="logout">
+          <Text style={{ fontSize: 18, fontWeight: 600 }}>Sign out</Text>
+        </Button>
       </TouchableOpacity>
     </ScrollView>
   )
