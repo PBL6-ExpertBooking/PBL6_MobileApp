7a5f7e17f215eeee463d02ba5cf2aa0100fa7cd6
Azanamin <minhnhat912002@gmail.com>
2023-10-10 21:28:09 +0700

    add expert info in profile

---
+++ screens/AccountInfo/components/UserExpertProfile/style.module.js
@@ -0,0 +1,43 @@
+import { StyleSheet } from 'react-native'
+
+export const styles = StyleSheet.create({
+  container: {
+    flex: 0,
+    alignItems: 'center',
+    width: '90%',
+    backgroundColor: '#F9F9F9',
+    borderRadius: 20,
+  },
+  profileContainer: {
+    gap: 10,
+  },
+  textInputContainer: {
+    width: '80%',
+    flexDirection: 'row',
+    gap: 10,
+  },
+  certificateContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    height: 50,
+    backgroundColor: '#D9D9D9',
+    borderRadius: 20,
+  },
+  buttonContainer: {
+    marginTop: 5,
+  },
+})
+
+export const textStyles = {
+  title: {
+    fontSize: 15,
+    fontWeight: 600,
+    alignSelf: 'flex-start',
+    marginLeft: '10%',
+    marginBottom: 10,
+  },
+  certificate: {
+    fontSize: 15,
+    fontWeight: 600,
+  },
+}
--- screens/AccountInfo/index.js
+++ screens/AccountInfo/index.js
@@ -1,19 +1,18 @@
-import React, { useContext, useState } from 'react'
-import { Text, TouchableOpacity, View } from 'react-native'
-import { Avatar, Button, TextInput } from 'react-native-paper'
+import React, { useContext } from 'react'
+import { ScrollView, Text, TouchableOpacity, View } from 'react-native'
+import { Avatar, Button } from 'react-native-paper'
 import { styles } from './style.module'
 import { AuthContext } from '../../contexts'
 
 import defaultAvatar from '../../assets/default-avatar.jpg'
+import UserProfile from './components/UserProfile'
+import UserExpertProfile from './components/UserExpertProfile'
 
 export default function AccountInfo() {
-  const { user } = useContext(AuthContext)
-
-  const [isEdit, setEdit] = useState(false)
-  const [userInfo, setUserInfo] = useState(user)
+  const { user, setUser } = useContext(AuthContext)
 
   return (
-    <View style={styles.container}>
+    <ScrollView contentContainerStyle={styles.container}>
       <View style={styles.avatarContainer}>
         <Avatar.Image source={defaultAvatar} size={200} />
         <TouchableOpacity>
@@ -22,91 +21,21 @@ export default function AccountInfo() {
           </Button>
         </TouchableOpacity>
       </View>
-      <View style={styles.userProfileContainer}>
-        <View style={styles.textInputContainer}>
-          <TextInput
-            mode="outlined"
-            label="first name"
-            value={userInfo.first_name}
-            editable={isEdit}
-            style={{ flex: 1 }}
-            dense
-            onChangeText={(text) =>
-              setUserInfo((userInfo) => ({ ...userInfo, first_name: text }))
-            }
-          />
-          <TextInput
-            mode="outlined"
-            label="last name"
-            value={userInfo.last_name}
-            editable={isEdit}
-            style={{ flex: 1 }}
-            dense
-            onChangeText={(text) =>
-              setUserInfo((userInfo) => ({ ...userInfo, last_name: text }))
-            }
-          />
-        </View>
-        <View style={styles.textInputContainer}>
-          <TextInput
-            mode="outlined"
-            label="username"
-            value={userInfo.username}
-            editable={isEdit}
-            style={{ width: '100%' }}
-            dense
-            onChangeText={(text) =>
-              setUserInfo((userInfo) => ({ ...userInfo, username: text }))
-            }
-          />
-        </View>
-        <View style={styles.textInputContainer}>
-          <TextInput
-            mode="outlined"
-            label="email"
-            value={userInfo.email}
-            editable={isEdit}
-            style={{ width: '100%' }}
-            dense
-            onChangeText={(text) =>
-              setUserInfo((userInfo) => ({ ...userInfo, email: text }))
-            }
-          />
-        </View>
-        <View style={{ width: '80%', marginTop: 10 }}>
-          {isEdit ? (
-            <View style={{ flexDirection: 'row', alignSelf: 'flex-end', gap: 10 }}>
-              <Button icon="pencil" mode="outlined">
-                Save Change
-              </Button>
-              <Button
-                icon="cancel"
-                textColor="white"
-                mode="contained"
-                onPress={() => setEdit(false)}
-              >
-                Cancel
-              </Button>
-            </View>
-          ) : (
-            <Button icon="pencil" mode="outlined" onPress={() => setEdit(true)}>
-              Edit Profile
-            </Button>
-          )}
-        </View>
-      </View>
+      <UserProfile />
       {user.role === 'USER' ? (
         <View style={styles.expertRegister}>
           <Text>You are not an Expert...</Text>
-          <TouchableOpacity>
+          <TouchableOpacity
+            onPress={() => setUser((user) => ({ ...user, role: 'EXPERT' }))}
+          >
             <Text style={{ color: '#1254FF', textDecorationLine: 'underline' }}>
               Be Expert !!!
             </Text>
           </TouchableOpacity>
         </View>
       ) : (
-        <Text>Expert profile</Text>
+        <UserExpertProfile />
       )}
-    </View>
+    </ScrollView>
   )
 }
