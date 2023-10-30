1b4ae8352bda608b69c42e359b940f496477d833
Azanamin <minhnhat912002@gmail.com>
2023-10-30 16:00:59 +0700

    fix profile UI

--- screens/AccountInfo/components/UserExpertProfile/style.module.js
+++ screens/AccountInfo/components/UserExpertProfile/style.module.js
@@ -2,9 +2,8 @@ import { StyleSheet } from 'react-native'
 
 export const styles = StyleSheet.create({
   container: {
-    flex: 0,
+    flex: 1,
     alignItems: 'center',
-    width: '90%',
     backgroundColor: '#F9F9F9',
     borderRadius: 20,
     marginBottom: 20,
