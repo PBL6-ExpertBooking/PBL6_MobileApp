f969f29c64e5691d85fad8c06c9c47ae7390ee0c
Azanamin <minhnhat912002@gmail.com>
2023-11-20 17:28:05 +0700

    integrated deposit API

--- services/transaction.js
+++ services/transaction.js
@@ -2,7 +2,14 @@ import { routes } from '../api'
 import { AxiosInterceptors } from '../utils'
 import { templatePopupOnRejection } from './common/template'
 
-export async function getDepositUrl() {}
+export async function getDeposit(amount) {
+  return await templatePopupOnRejection(async () => {
+    const response = await AxiosInterceptors.post(routes.transaction.deposit, {
+      amount,
+    })
+    return response
+  })
+}
 
 export async function getPayment(job_request_id) {
   return await templatePopupOnRejection(async () => {
