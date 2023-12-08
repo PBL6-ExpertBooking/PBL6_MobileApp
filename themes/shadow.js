a2babdc29c0b537089544bd765abd3cc8dc7215d
Azanamin <minhnhat912002@gmail.com>
2023-12-08 14:19:22 +0700

    change template popup

---
+++ themes/shadow.js
@@ -0,0 +1,35 @@
+export const light = {
+  shadowColor: '#000',
+  shadowOffset: {
+    width: 0,
+    height: 1,
+  },
+  shadowOpacity: 0.22,
+  shadowRadius: 2.22,
+
+  elevation: 3,
+}
+
+export const medium = {
+  shadowColor: '#000',
+  shadowOffset: {
+    width: 0,
+    height: 3,
+  },
+  shadowOpacity: 0.27,
+  shadowRadius: 4.65,
+
+  elevation: 6,
+}
+
+export const thick = {
+  shadowColor: '#000',
+  shadowOffset: {
+    width: 0,
+    height: 5,
+  },
+  shadowOpacity: 0.34,
+  shadowRadius: 6.27,
+
+  elevation: 10,
+}
--- utils/popup/success.js
+++ utils/popup/success.js
@@ -1,11 +1,20 @@
 import { Popup } from 'react-native-popup-confirm-toast'
+import { colors } from '../../themes'
 
-export function popupMessage({ title, showIcon, message }) {
+export function popupMessage({
+  title,
+  showIcon,
+  message,
+  callback = () => Popup.hide(),
+}) {
   Popup.show({
     type: 'success',
     title,
     iconEnabled: showIcon,
     textBody: message,
+    buttonText: 'Ok',
+    okButtonStyle: { backgroundColor: colors.secondary },
+    callback,
     bounciness: 0,
     duration: 30,
     closeDuration: 50,
