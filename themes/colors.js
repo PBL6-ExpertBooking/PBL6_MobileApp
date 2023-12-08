9d734a62125f54472f1b19ffb00a43720b9c147b
Azanamin <minhnhat912002@gmail.com>
2023-12-08 12:49:51 +0700

    change color scheme

--- themes/colors.js
+++ themes/colors.js
@@ -1,5 +1,14 @@
 export const colors = {
-  primaryColor: '#FF8686',
-  secondaryColor: '#E8DDDD',
-  backgroundColor: '#FFFFFF',
+  primary: '#2196F3',
+  secondary: '#03A9F4',
+  accent: '#FF9800',
+  background: '#FFFFFF',
+  surface: '#F0F0F0',
+  text: '#000000',
+  onBackground: '#000000',
+  onSurface: '#000000',
+  disabled: '#9E9E9E',
+  placeholder: '#9E9E9E',
+  backdrop: '#000000',
+  inputBackground: '#FAFAFA',
 }
