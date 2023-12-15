ccf96e1b8298021ab6425d7c3dcd22b9f51503b2
Azanamin <minhnhat912002@gmail.com>
2023-12-15 12:22:28 +0700

    add job request accepted notification

--- screens/general/MainScreen/SubScreens/Notifications/components/NotificationItem/index.js
+++ screens/general/MainScreen/SubScreens/Notifications/components/NotificationItem/index.js
@@ -1,11 +1,14 @@
 import React from 'react'
 import { NOTIFICATION } from '../../../../../../../constants'
 import NewJobRequest from './NewJobRequest'
+import JobRequestAccepted from './JobRequestAccepted'
 
 export default function NotificationItem({ item }) {
   const { type } = item
 
   if (type === NOTIFICATION.TYPE.NEW_JOB_REQUEST)
     return <NewJobRequest item={item} />
-  else return <></>
+  else if (type === NOTIFICATION.TYPE.JOB_REQUEST_ACCEPTED)
+    return <JobRequestAccepted item={item} />
+  return <></>
 }
